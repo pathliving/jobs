4d2b90bf2353e75d6f650264aa2261385d9cbcfc
Andrii Hrynenko <andriihrynenko@gmail.com>
2023-09-17 22:22:55 +0300

    refactor: convert fonts helper into hook

--- helpers/getFont.js
+++
@@ -1,5 +0,0 @@
-const getFont = (name, extension) => {
-  return require(`../assets/fonts/${name}.${extension}`)
-}
-
-export default getFont;
---
+++ hooks/useStaticFonts.js
@@ -0,0 +1,17 @@
+import {useFonts} from "expo-font";
+import {FONTS} from "../constants/theme";
+
+const useStaticFonts = () => {
+    const [fontsLoaded, fontError] = useFonts({
+        [FONTS.regular]: require('../assets/fonts/DMSans-Regular.ttf'),
+        [FONTS.medium]: require('../assets/fonts/DMSans-Medium.ttf'),
+        [FONTS.bold]: require('../assets/fonts/DMSans-Bold.ttf'),
+    });
+
+    return [
+        fontsLoaded,
+        fontError,
+    ]
+};
+
+export default useStaticFonts;
